55c25d77a654b50733f1a6fd57eeadf414da10ec
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-15 09:20:22 +0530

    quotient

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿namespace quotientreaminder213
+{
+    public static class quotient
+    {
+        public static void Main(string[] args)
+        {
+            int dividend = 33, divisor = 3;
+
+            int quotient = dividend / divisor;
+            int remainder = dividend % divisor;
+
+            Console.WriteLine("dividend:{0} divisor:{1}", dividend, divisor);
+            Console.WriteLine("quotient = " + quotient);
+            Console.WriteLine("remainder = " + remainder);
+            Console.ReadLine();
+        }
+    }
+}
